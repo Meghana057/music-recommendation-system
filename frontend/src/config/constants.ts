8ca06dc28c45600d6d5d160eaf4d52ecba1692a8
Meghana057 <meghanajagadish05@gmail.com>
2025-08-04 10:54:13 -0400

    added health check variable for prod

--- frontend/src/config/constants.ts
+++ frontend/src/config/constants.ts
@@ -1,2 +1,2 @@
 export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api/v1';
-export const HEALTH_CHECK_URL = 'http://localhost:8000/health';
+export const HEALTH_CHECK_URL =(API_BASE_URL?.replace(/\/api\/v1$/, '') || 'http://localhost:8000') + '/health';
